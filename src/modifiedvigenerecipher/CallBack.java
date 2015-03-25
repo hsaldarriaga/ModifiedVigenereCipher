ad47ec7096a10da174e2110fe15999ce4847a151
Laboratorio <Laboratorio@LR-10>
2015-03-25 15:46:30 -0500

    Second Commit

---
+++ src/modifiedvigenerecipher/CallBack.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modifiedvigenerecipher;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public interface CallBack {
+    public void onFinish(Decoder.Answer answer);
+}
