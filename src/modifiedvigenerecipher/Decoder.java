bfda3afced1edbc315527f02bf2be7d56c2f966a
Hernan Saldarriaga <hsaldarriaga@uninorte.edu.co>
2015-03-28 17:30:06 -0500

    Change 1.6

--- src/modifiedvigenerecipher/Decoder.java
+++ src/modifiedvigenerecipher/Decoder.java
@@ -5,6 +5,9 @@
 package modifiedvigenerecipher;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import javax.swing.JLabel;
 import javax.swing.JProgressBar;
 import javax.swing.SwingUtilities;
@@ -13,10 +16,11 @@ import javax.swing.SwingUtilities;
  *
  * @author Laboratorio
  */
-public class Decoder {
-    JProgressBar bar;
-    JLabel label;
-    CallBack call;
+public class Decoder implements Runnable{
+    private final JProgressBar bar;
+    private final JLabel label;
+    private final CallBack call;
+    private Byte[] data;
     
     public Decoder(JProgressBar bar, JLabel label, CallBack call) {
         this.bar = bar;
@@ -24,64 +28,37 @@ public class Decoder {
         this.call = call;
     }
     
-    public void getTextAsyncronous(final byte[] data){
-        Thread hilo = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                final Answer answer = new Answer();
-                //Code
-                SetText("Preparando Buffer de Valores...");
-                UpgradeValue(5);
-                ArrayList<Byte> key_pos_let = new ArrayList<Byte>();
-                for (int i = 0; i < 128; i++) {
-                    key_pos_let.add((byte)i);
-                }
-                boolean found = false;
-                int size = 1;
-                while (!found) {
-                    for (int j = 0; j < key_pos_let.size(); j++) {
-                        int i=0;
-                        while (i*size < data.length) {
-                            byte resu = (byte) (data[i*size] ^ key_pos_let.get(j));
-                            i++;
-                        }
-                    }
-                    
-                    size++;
-                }
-                //Code
-                SwingUtilities.invokeLater(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        call.onFinish(answer);
-                    }
-                });
-            }
-        });
+    public void getTextAsyncronous(byte[] data){
+        this.data = new Byte[data.length];
+        for (int i = 0; i < data.length; i++) {
+            this.data[i] = data[i];
+        }
+        label.setText("Preparing Data...");
+        bar.setValue(0);
+        Thread hilo = new Thread(this);
         hilo.start();
     }
     
-    private void SetText(final String text){
+    private void SetText(final String text, final int value){
         SwingUtilities.invokeLater(new Runnable() {
 
             @Override
             public void run() {
-                label.setText(text);
+                if (text!= null)
+                    label.setText(text);
+                if (value > 0)
+                    bar.setValue(value);
             }
         });
+    }
+
+    @Override
+    public void run() {
+        //Aqui se escribe el metodo para decifrarlo, la palabra cifrada es la variable 'data' es un vector
+        // de bytes
+        
     }
     
-    private void UpgradeValue(final int value) {
-        SwingUtilities.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                bar.setValue(value);
-            }
-        });
-    }
     
     public static class Answer{
         public String Text;
